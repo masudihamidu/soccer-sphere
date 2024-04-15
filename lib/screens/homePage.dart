58e0c72090d28969b22e8f9fb4ea33f7f0e4605a
masoud hamidu <masudihmfaume@gmail.com>
2024-04-15 17:27:55 +0300

    implementing search textfield

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,6 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import 'NavBar.dart';
 
@@ -13,15 +12,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _showSearchField = false; // State variable for showing search field
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer:  NavBar(),
+      drawer: NavBar(),
       appBar: AppBar(
-        // Apply linear gradient to AppBar background
         backgroundColor: Colors.transparent, // Make the background transparent
         flexibleSpace: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
@@ -29,32 +29,58 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white,),
+            onPressed: () {
+              setState(() {
+                _showSearchField = true;
+              });
+            },
+          ),
+        ],
+        title: _showSearchField ? SearchField() : null, // Show search field if _showSearchField is true
       ),
-      body: content()
+      body: content(),
     );
   }
 
-  Widget content(){
+  Widget content() {
     return Container(
       child: CarouselSlider(
-        items: [1,2,3,4,5].map((i) {
+        items: [1, 2, 3, 4, 5].map((i) {
           return Container(
             width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.symmetric(horizontal: 5),
+            margin: const EdgeInsets.symmetric(horizontal: 5),
             decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(10)
-            ),
+                color: Colors.green, borderRadius: BorderRadius.circular(10)),
             child: Center(
-              child:   Text("text $i", style: TextStyle(fontSize: 40),),
-            )
+              child: Text(
+                "text $i",
+                style: TextStyle(fontSize: 40),
+              ),
+            ),
           );
         }).toList(),
-        options: CarouselOptions(
-          height: 300
-        ),
-      )
+        options: CarouselOptions(height: 250),
+      ),
+    );
+  }
+}
 
+class SearchField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          hintStyle: TextStyle(color: Colors.white),
+          border: InputBorder.none,
+        ),
+        style: TextStyle(color: Colors.white),
+      ),
     );
   }
 }
